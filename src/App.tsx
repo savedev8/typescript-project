37ccee497b9ffe8675dee86d39e807935d06b743
savedev8 <on.savedev8@gmail.com>
2023-07-14 16:22:40 +0300

    feat: added themes toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import './index.scss';
+import './styles/index.scss';
 
 import { Link } from "react-router-dom";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
+
 
 export const App = () => {
+    const {theme, toggleTheme} = useTheme();
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={() => toggleTheme()}>TOGGLE</button>
             <Link to="/">Главная</Link>
             <Link to="/about">Инфо</Link>
             <Suspense fallback={<div>Loading...</div>}>
---
+++ src/theme/useTheme.tsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+
+interface UseThemeResult {
+    toggleTheme: () => void;
+    theme: Theme;
+}
+
+export const useTheme = (): UseThemeResult => { 
+    const {theme, setTheme} = useContext(ThemeContext);
+
+    const toggleTheme = () => {
+        const defaultTheme = theme === Theme.DARK? Theme.LIGHT : Theme.DARK;
+        setTheme(defaultTheme);
+        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, defaultTheme);
+    }
+    return {toggleTheme, theme};
+}
