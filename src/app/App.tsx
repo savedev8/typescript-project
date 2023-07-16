07f944fc29d5243349f329399c658abfa6421afc
savedev8 <on.savedev8@gmail.com>
2023-07-16 12:11:19 +0300

    refactor: changed AppRouter and added AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,19 @@
-import { Route, Routes } from "react-router-dom";
 import './styles/index.scss';
 
-import { Link } from "react-router-dom";
-import { Suspense } from "react";
 import { useTheme } from "./providers/ThemeProvider/lib/useTheme";
 
-import AboutPage from "pages/AboutPage/ui/AboutPage";
-import MainPage from "pages/MainPage/ui/MainPage";
 import { classNames } from "shared/lib/classNames/classNames";
+import AppRouter from "./providers/router/ui/AppRouter";
+import { Navbar } from 'widgets/Navbar';
 
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <AppRouter />
             <button onClick={() => toggleTheme()}>TOGGLE</button>
-            <Link to="/">Главная</Link>
-            <Link to="/about">Инфо</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path="/" element={<MainPage />} />
-                    <Route path="/about" element={<AboutPage />} />
-                </Routes>
-            </Suspense>
         </div>
     )
 }
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,19 @@
+import AboutPage from "pages/AboutPage/ui/AboutPage";
+import MainPage from "pages/MainPage/ui/MainPage";
+import { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { routeConfig } from "shared/config/routeConfig/routeConfig";
+
+export default function AppRouter() {
+    return (
+        <div>    
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                {Object.values(routeConfig).map(({element, path}) => (
+                    <Route key={path} path={path} element={element} />
+                ))}
+            </Routes>
+        </Suspense>
+        </div>
+    )
+}
