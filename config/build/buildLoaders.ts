838089b6bbe5fc862d7d0a4912d79b3b80a1154d
savedev8 <on.savedev8@gmail.com>
2023-07-20 10:29:59 +0300

    added babel and example of using

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -10,6 +10,20 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         use: ['@svgr/webpack'],
     }
 
+    const babelLoader =  {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+            presets: ['@babel/preset-env'],
+            "plugins": [
+                ["i18next-extract", { locales: ["ru", "en"], keyAsDefaultValue: true }],
+                ],
+            }
+        }
+    }
+
     const cssLoader =
     {
         test: /\.s[ac]ss$/i,
@@ -47,6 +61,7 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ]
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -17,8 +17,6 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-
-        new webpack.HotModuleReplacementPlugin(),
     ]
     if (isDev) {
         plugins.push(new ReactRefreshWebpackPlugin());
