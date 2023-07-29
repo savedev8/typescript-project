893cfa944b6936e32b3821ae64b8963b4b3877f8
savedev8 <on.savedev8@gmail.com>
2023-07-29 06:27:58 +0300

    fix

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,15 @@
 import { BugButton } from 'app/providers/ErrorBoundary';
 import { useTranslation } from 'react-i18next';
+import { Loader } from 'shared/ui/Loader/Loader';
+import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
     return (
-        <>
-            <BugButton />
-            <div>{t('Главная')}</div>
-        </>
+        <div>
+            {/* {t('Главная')} */}
+            <Loader />
+        </div>
     );
 };
 
