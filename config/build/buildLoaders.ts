ff7552d40148c58d264bae0afa1430ac1380a49c
savedev8 <on.savedev8@gmail.com>
2023-07-29 07:01:00 +0300

    fix

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,5 @@
 import webpack from 'webpack';
 
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
 import { buildCssLoader } from './loaders/buildCssLoader';
 
@@ -39,7 +38,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     };
 
     const fileLoader = {
-        test: /\.(png|jpe?g|gif)$/i,
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
         use: [
             {
                 loader: 'file-loader',
--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -4,22 +4,19 @@ export function buildCssLoader(isDev: boolean) {
     return {
         test: /\.s[ac]ss$/i,
         use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader, // Creates `style` nodes from JS strings
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             {
-                loader: 'css-loader', // Translates CSS into CommonJS
+                loader: 'css-loader',
                 options: {
                     modules: {
-                        auto: (resPath: string) => Boolean(
-                            resPath.includes('.module.'),
-                        ),
-                        localIdentName:
-                            isDev
-                                ? '[path][name]__[local]--[hash:base64:5]'
-                                : '[hash:base64:8]',
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]',
                     },
                 },
             },
-            'sass-loader', // Compiles Sass to CSS
+            'sass-loader',
         ],
     };
 }
