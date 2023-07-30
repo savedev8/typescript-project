8bba93ac939cb1b638152d3d8812146839596c26
savedev8 <on.savedev8@gmail.com>
2023-07-30 11:40:25 +0300

    fix

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -3,12 +3,12 @@ import { Button, ThemeButton } from './Button';
 
 describe('Test render', () => {
     test('with only first param', () => {
-        render(<Button />);
+        render(<Button></Button>);
         expect(screen.getByText('TEST')).toBeInTheDocument();
     });
 
     test('Test clear theme', () => {
-        render(<Button theme={ThemeButton.CLEAR}/>);
+        render(<Button theme={ThemeButton.CLEAR}></Button>);
         expect(screen.getByText('TEST')).toHaveClass('clear');
         screen.debug();
     });
