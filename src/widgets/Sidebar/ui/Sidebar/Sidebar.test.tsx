35341d77ada03d22128b79ca36841082b941b381
savedev8 <on.savedev8@gmail.com>
2023-08-05 11:07:31 +0300

    fix ts linter

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,7 +1,7 @@
 import { fireEvent, screen } from '@testing-library/react';
 
-import { Sidebar } from './Sidebar';
 import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
+import { Sidebar } from './Sidebar';
 
 describe('Sidebar', () => {
     test('with only first param', () => {
