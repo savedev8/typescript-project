3a25a6c62f0eada945001e1f7f9ae6c82c3c2522
savedev8 <on.savedev8@gmail.com>
2023-08-11 16:25:25 +0300

    commit with ts error

--- src/app/providers/StoreProvider/ui/StoreProvider.ts
+++ src/app/providers/StoreProvider/ui/StoreProvider.ts
@@ -1,15 +1,19 @@
 import { ReactNode } from "react";
-import { Provider } from "react-redux";
+import { createReduxStore } from "../config/store";
+import { StateSchema } from "../config/StateSchema";
 
 interface StoreProviderProps {
     children?: ReactNode;
+    initialState?: StateSchema;
 }
 
 export const StoreProvider = (props: StoreProviderProps) => {
-    const { children } = props;
+    const { children, initialState } = props;
+
+    const store = createReduxStore(initialState);
     return (
-        <Provider store={}>
-            { children }
-        </Provider>
-    )
+        <Provider store= { store } >
+        { children }
+        < /Provider>
+    );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import { ErrorBoundary } from './app/providers/ErrorBoundary';
 import { App } from './app/App';
 import { ThemeProvider } from './app/providers/ThemeProvider/ui/ThemeProvider';
@@ -8,12 +9,14 @@ import './app/styles/index.scss';
 import './shared/i18n/i18n';
 
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     document.getElementById('root'),
 );
