097eafce5622e5f2c6eb79ecdc67d4855e41d178
savedev8 <on.savedev8@gmail.com>
2023-08-14 10:26:23 +0300

    added changes with redux toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { StoreProvider } from 'app/providers/StoreProvider';
-import { ErrorBoundary } from './app/providers/ErrorBoundary';
-import { App } from './app/App';
 import { ThemeProvider } from './app/providers/ThemeProvider/ui/ThemeProvider';
+import { App } from './app/App';
 import './app/styles/index.scss';
-
 import './shared/i18n/i18n';
+import { ErrorBoundary } from './app/providers/ErrorBoundary';
 
 render(
     <StoreProvider>
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,14 +1,12 @@
-import { BugButton } from 'app/providers/ErrorBoundary';
+import { Counter } from 'entities/Counter';
 import { useTranslation } from 'react-i18next';
-import { Loader } from 'shared/ui/Loader/Loader';
-import { PageLoader } from 'shared/ui/PageLoader/PageLoader';
 
 const MainPage = () => {
     const { t } = useTranslation('main');
     return (
         <div>
-            {/* {t('Главная')} */}
-            <Loader />
+            {t('Главная')}
+            <Counter />
         </div>
     );
 };
