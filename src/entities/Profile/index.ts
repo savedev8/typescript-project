990773c1c632dd15b19c825f40ac1a7a4150628a
savedev8 <on.savedev8@gmail.com>
2023-09-21 16:17:04 +0300

    fix

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -13,3 +13,7 @@ export { fetchProfileData } from './model/services/fetchProfileData/fetchProfile
 export {
     ProfileCard,
 } from './ui/ProfileCard/ProfileCard';
+
+export { getProfileData } from './model/selectors/getProfileData/getProfileData';
+export { getProfileIsLoading } from './model/selectors/getProfileIsLoading/getProfileIsLoading';
+export { getProfileError } from './model/selectors/getProfileError/getProfileError';
