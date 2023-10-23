a389b7cf741fdbd3de5acef58a84d68282d72508
savedev8 <on.savedev8@gmail.com>
2023-10-23 16:22:26 +0300

    fix auth

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,15 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInited, userActions } from 'entities/User';
 import AppRouter from './providers/router/ui/AppRouter';
 import { useTheme } from './providers/ThemeProvider/lib/useTheme';
 
 export const App = () => {
     const { theme } = useTheme();
     const dispatch = useDispatch();
+    const inited = useSelector(getUserInited);
 
     useEffect(() => {
         dispatch(userActions.initAuthData());
@@ -21,7 +22,7 @@ export const App = () => {
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
-                    <AppRouter />
+                    {inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,3 +1,16 @@
-export { userReducer, userActions } from './model/slice/userSlice';
-export { UserSchema, User } from './model/types/user';
-export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
+export {
+    getUserAuthData,
+} from './model/selectors/getUserAuthData/getUserAuthData';
+
+export {
+    getUserInited,
+} from './model/selectors/getUserInited/getUserInited';
+
+export {
+    userReducer,
+    userActions,
+} from './model/slice/userSlice';
+export {
+    UserSchema,
+    User,
+} from './model/types/user';
