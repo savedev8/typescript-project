4ef85a5bc34a636247a5f729dc2a1b23f4ee4607
savedev8 <on.savedev8@gmail.com>
2024-01-30 17:45:26 +0300

    add settings

--- build/221.0b13eb81c9a0bf13821e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproduction_project=self.webpackChunkproduction_project||[]).push([[221],{221:(r,c,e)=>{e.r(c),e.d(c,{default:()=>s});var t=e(893),n=e(499);const s=function(){var r=(0,n.$)("about").t;return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)("div",{children:r("О сайте")}),(0,t.jsx)("div",{children:r("О сайте c плагином")})]})}}}]);
