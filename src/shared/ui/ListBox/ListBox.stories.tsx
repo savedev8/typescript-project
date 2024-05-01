5c6092834e236493e6b82796fed55446057ec333
savedev8 <on.savedev8@gmail.com>
2024-05-01 14:28:38 +0300

    add dropdown

--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -10,7 +10,56 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Normal: Story = {};
-Normal.args = {
+export const Normal: Story = {
+    args:   {
+    value: '123',
+    items: [
+        { content: '1asfasfasf23', value: '123' },
+        { content: '1asfasfasf21233', value: '1232' },
+    ],
+    }
+};
 
+export const topLeft: Story = {
+    args: {
+    direction: 'top left',
+    value: '123',
+    items: [
+        { content: '1asfasfasf23', value: '123' },
+        { content: '1asfasfasf21233', value: '1232' },
+    ],
+}
+};
+
+export const topRight: Story = {
+    args: {
+    direction: 'top right',
+    value: '123',
+    items: [
+        { content: '1asfasfasf23', value: '123' },
+        { content: '1asfasfasf21233', value: '1232' },
+    ],
+}
+};
+
+export const bottomLeft: Story = {
+    args: {
+    direction: 'bottom left',
+    value: '123',
+    items: [
+        { content: '1asfasfasf23', value: '123' },
+        { content: '1asfasfasf21233', value: '1232' },
+    ],
+}
+};
+
+export const bottomRight: Story = {
+    args: {
+    direction: 'bottom right',
+    value: '123',
+    items: [
+        { content: '1asfasfasf23', value: '123' },
+        { content: '1asfasfasf21233', value: '1232' },
+    ],
+}
 };
