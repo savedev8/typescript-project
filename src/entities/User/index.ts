671d280a62ed03c3a5362ec8302634667ff8e076
savedev8 <on.savedev8@gmail.com>
2024-05-05 17:50:25 +0300

    fix: add test

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -7,10 +7,15 @@ export {
 } from './model/selectors/getUserInited/getUserInited';
 
 export {
+    isUserAdmin, isUserManager, isUserAdvocate, getUserRoles,
+} from './model/selectors/roleSelectors';
+
+export {
     userReducer,
     userActions,
 } from './model/slice/userSlice';
 export {
     UserSchema,
     User,
+    UserRole,
 } from './model/types/user';
