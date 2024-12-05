ffa449dc085658bb2a4dc2261370737e06bdcf00
savedev8 <on.savedev8@gmail.com>
2024-12-05 16:41:35 +0300

    fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,8 @@
 cd ~/typescript-project
 npm run build:prod
 
-rm -rf ~/../var/www/typescript-project/html
-mv ~/typescript-project/build ~/var/www/typescript-project/html # вынести пути в отдельные переменные
+sudo rm -rf ~/../../var/www/typescript-project/html
+sudo mv ~/typescript-project/build ~/../../var/www/typescript-project/html 
+
+# вынести пути в отдельные переменные
+# разобраться с правами
