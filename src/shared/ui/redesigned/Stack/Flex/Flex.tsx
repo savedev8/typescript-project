b236e74d74262600f3911ecf80ea4646e369c950
savedev8 <on.savedev8@gmail.com>
2024-12-13 14:03:39 +0300

    fix redesign list of articles

--- src/shared/ui/redesigned/Stack/Flex/Flex.tsx
+++ src/shared/ui/redesigned/Stack/Flex/Flex.tsx
@@ -1,36 +1,37 @@
-import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Flex.module.scss';
 
 export type FlexJustify = 'start' | 'center' | 'end' | 'between';
 export type FlexAlign = 'start' | 'center' | 'end';
 export type FlexDirection = 'row' | 'column';
+export type FlexWrap = 'nowrap' | 'wrap';
 export type FlexGap = '4' | '8' | '16' | '24' | '32';
 
 const justifyClasses: Record<FlexJustify, string> = {
-    start: cls?.justifyStart,
-    center: cls?.justifyCenter,
-    end: cls?.justifyEnd,
-    between: cls?.justifyBetween,
+    start: cls.justifyStart,
+    center: cls.justifyCenter,
+    end: cls.justifyEnd,
+    between: cls.justifyBetween,
 };
 
 const alignClasses: Record<FlexAlign, string> = {
-    start: cls?.alignStart,
-    center: cls?.alignCenter,
-    end: cls?.alignEnd,
+    start: cls.alignStart,
+    center: cls.alignCenter,
+    end: cls.alignEnd,
 };
 
 const directionClasses: Record<FlexDirection, string> = {
-    row: cls?.directionRow,
-    column: cls?.directionColumn,
+    row: cls.directionRow,
+    column: cls.directionColumn,
 };
 
 const gapClasses: Record<FlexGap, string> = {
-    4: cls?.gap4,
-    8: cls?.gap8,
-    16: cls?.gap16,
-    24: cls?.gap24,
-    32: cls?.gap32,
+    4: cls.gap4,
+    8: cls.gap8,
+    16: cls.gap16,
+    24: cls.gap24,
+    32: cls.gap32,
 };
 
 type DivProps = DetailedHTMLProps<
@@ -44,6 +45,7 @@ export interface FlexProps extends DivProps {
     justify?: FlexJustify;
     align?: FlexAlign;
     direction: FlexDirection;
+    wrap?: FlexWrap;
     gap?: FlexGap;
     max?: boolean;
 }
@@ -55,6 +57,7 @@ export const Flex = (props: FlexProps) => {
         justify = 'start',
         align = 'center',
         direction = 'row',
+        wrap = 'nowrap',
         gap,
         max,
         ...otherProps
@@ -65,6 +68,7 @@ export const Flex = (props: FlexProps) => {
         justifyClasses[justify],
         alignClasses[align],
         directionClasses[direction],
+        cls[wrap],
         gap && gapClasses[gap],
     ];
 
