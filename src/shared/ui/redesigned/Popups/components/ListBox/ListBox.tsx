db8814ba129664ff9f20a7b95c3435641eeb67d3
savedev8 <on.savedev8@gmail.com>
2024-12-13 12:23:46 +0300

    add redesign input and button with arrows

--- src/shared/ui/redesigned/Popups/components/ListBox/ListBox.tsx
+++ src/shared/ui/redesigned/Popups/components/ListBox/ListBox.tsx
@@ -7,6 +7,8 @@ import { Button } from '../../../Button/Button';
 import cls from './ListBox.module.scss';
 import { mapDirectionClass } from '../../styles/consts';
 import popupCls from '../../styles/popup.module.scss';
+import ArrowIcon from '@/shared/assets/icons/arrow-bottom.svg';
+import { Icon } from '../../../Icon';
 
 export interface ListBoxItem<T extends string> {
     value: string;
@@ -56,8 +58,12 @@ export function ListBox<T extends string>(props: ListBoxProps<T>) {
                 value={value}
                 onChange={onChange}
             >
-                <HListBox.Button  className={cls.trigger}>
-                    <Button variant="filled" disabled={readonly}>
+                <HListBox.Button className={cls.trigger}>
+                    <Button
+                        variant="filled"
+                        disabled={readonly}
+                        addonRight={<Icon Svg={ArrowIcon} />}
+                    >
                         {selectedItem?.content ?? defaultValue}
                     </Button>
                 </HListBox.Button>
