ae5bf32a71cda83fb71d35b511f2e88c6c03afc0
savedev8 <on.savedev8@gmail.com>
2024-12-17 12:34:48 +0300

    redesign auth modal, fixes

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -1,34 +1,34 @@
-import { Meta, StoryObj } from '@storybook/react';
-import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
-import LoginForm from './LoginForm';
+// import { Meta, StoryObj } from '@storybook/react';
+// import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+// import LoginForm from './LoginForm';
 
-const meta = {
-    title: 'features/LoginForm',
-    component: LoginForm,
-} satisfies Meta<typeof LoginForm>;
+// const meta = {
+//     title: 'features/LoginForm',
+//     component: LoginForm,
+// } satisfies Meta<typeof LoginForm>;
 
-export default meta;
-type Story = StoryObj<typeof meta>;
+// export default meta;
+// type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
-Primary.decorators = [
-    StoreDecorator({
-        loginForm: { username: '123', password: '123' },
-    }),
-];
+// export const Primary: Story = {};
+// Primary.decorators = [
+//     StoreDecorator({
+//         loginForm: { username: '123', password: '123' },
+//     }),
+// ];
 
-export const WithError: Story = {};
-WithError.args = {};
-WithError.decorators = [
-    StoreDecorator({
-        loginForm: { username: '123', password: 'asd', error: 'ERROR' },
-    }),
-];
+// export const WithError: Story = {};
+// WithError.args = {};
+// WithError.decorators = [
+//     StoreDecorator({
+//         loginForm: { username: '123', password: 'asd', error: 'ERROR' },
+//     }),
+// ];
 
-export const Loading: Story = {};
-Loading.args = {};
-Loading.decorators = [
-    StoreDecorator({
-        loginForm: { isLoading: true },
-    }),
-];
+// export const Loading: Story = {};
+// Loading.args = {};
+// Loading.decorators = [
+//     StoreDecorator({
+//         loginForm: { isLoading: true },
+//     }),
+// ];
--- src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
+++ src/features/AuthByUsername/ui/LoginModal/LoginModal.tsx
@@ -1,6 +1,7 @@
-import { classNames } from '@/shared/lib/classNames/classNames';
-import { Modal } from '@/shared/ui/redesigned/Modal';
 import { Suspense } from 'react';
+import { Modal } from '@/shared/ui/redesigned/Modal';
+import { classNames } from '@/shared/lib/classNames/classNames';
+import { Loader } from '@/shared/ui/deprecated/Loader';
 import { LoginFormAsync } from '../LoginForm/LoginForm.async';
 
 interface LoginModalProps {
@@ -11,12 +12,12 @@ interface LoginModalProps {
 
 export const LoginModal = ({ className, isOpen, onClose }: LoginModalProps) => (
     <Modal
-        className={classNames('', {}, [className || ''])}
+        className={classNames('', {}, [className])}
         isOpen={isOpen}
         onClose={onClose}
         lazy
     >
-        <Suspense fallback="">
+        <Suspense fallback={<Loader />}>
             <LoginFormAsync onSuccess={onClose} />
         </Suspense>
     </Modal>
